51033cbf9849840ca49cd53a8bcaa73ea82aecfc
AlphaSheep <brendan@alphasheep.co.za>
2023-12-29 07:28:06 +0200

    Create initial project skeleton

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function App() {
+
+  return <>
+    <div className="container">
+      <h1>Hello World</h1>
+    </div>
+  </>
+
+}
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import App from "./App";
+import "./styles.less";
+
+var container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);

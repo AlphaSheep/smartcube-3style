df875bc897da40ebabf2ecd16b96df9a54f83108
AlphaSheep <brendan@alphasheep.co.za>
2023-12-29 17:08:29 +0200

    Add simple POC with connect and reset buttons and a simple timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React, { useState } from 'react';
 
+import Timer from './components/timer';
+
+import getCubeService from './services/bt-cube';
+import getAlgBuilderService from './services/alg-builder';
+
 export default function App() {
+
+  const btCubeService = getCubeService();
+  const algBuilder = getAlgBuilderService();
+
+  const onConnect = () => {
+    btCubeService.connect();
+  };
+
+  const onReset = () => {
+    algBuilder.reset();
+  }
 
   return <>
     <div className="container">
       <h1>Hello World</h1>
+
+      <Timer prompt="AB" saveTime={null} />
+
+      <button onClick={onConnect}>Connect</button>
+
+      <button onClick={onReset}>Reset</button>
     </div>
   </>
 
---
+++ src/components/timer.tsx
@@ -0,0 +1,72 @@
+import React, {useState, useEffect} from "react";
+
+export default function Timer({prompt, saveTime}) {
+  const [startTime, setStart] = useState(0);
+  const [time, setTime] = useState(undefined);
+
+  const [isRunning, setRunning] = useState(false);
+  const [isArmed, setArmed] = useState(false);
+
+  const updateTime = () => {
+    setTime(Date.now() - startTime);
+  }
+
+  const getCurrentTimeString = () => {
+    if (!time)
+      return "---";
+
+    return (time / 1000).toFixed(2);
+  }
+
+  const onClick = () => {
+    if (isRunning) {
+      setRunning(false);
+      // saveTime(getCurrentTime());
+    } else {
+      setStart(Date.now());
+      setRunning(true);
+    }
+  }
+
+  const onKeyUp = (event) => {
+    if (isArmed && !isRunning && event.key === ' ') {
+      onClick();
+      setArmed(false);
+    }
+  }
+
+  const onKeyDown = (event) => {
+    if (event.key === ' ' && !event.repeat) {
+      if (isRunning) {
+        onClick();
+      } else {
+        setArmed(true);
+        setTime(undefined)
+      }
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keyup', onKeyUp);
+    window.addEventListener('keydown', onKeyDown);
+    return () => {
+      window.removeEventListener('keyup', onKeyUp);
+      window.removeEventListener('keydown', onKeyDown);
+    }
+  });
+
+  useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        updateTime();
+      }, 30);
+      return () => clearInterval(interval);
+    }
+  });
+
+  return (
+    <div className="timer-card" onClick={onClick}>
+      <p>{prompt}</p>
+      <p>{getCurrentTimeString()}</p>
+    </div>);
+}

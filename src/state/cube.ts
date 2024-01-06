57895fc36e11fbe533f35dcb662c4863bc2feb59
AlphaSheep <brendan@alphasheep.co.za>
2024-01-06 15:40:59 +0200

    Connect cube redux slice to bluetooth cube service

--- src/state/cube.ts
+++ src/state/cube.ts
@@ -1,30 +1,60 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, EnhancedStore, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from './store';
 
-import { getSolvedState, applyMove, CubeState } from '../lib/cube/cube';
+import { getSolvedState, applyMove, areStatesEqual, CubeState } from '../lib/cube/cube';
+import getCubeService from "../services/bluetooth-cube";
 
 // Type definitions
+export type Cube = {
+  cubeState: CubeState;
+  moves: TimestampedMove[];
+  target?: CubeState;
+};
+
+export type TimestampedMove = {
+  move: string;
+  timestamps: [number, number];
+};
+
 
 // Initial State
-const initialState: CubeState = getSolvedState();
+const initialState: Cube = {
+  cubeState: getSolvedState(),
+  moves: [],
+};
 
 // Selectors
 export const selectCubeState = (state: RootState) => state.cube;
+export const selectMoves = (state: RootState) => state.cube.moves;
+export const selectIsReachedTarget = (state: RootState) =>
+  state.cube.target ? areStatesEqual(state.cube.cubeState, state.cube.target) : false;
 
 // Slice
 export const cubeSlice = createSlice({
   name: 'cube',
   initialState,
   reducers: {
-    applyTurn: (state, action: PayloadAction<string>) => {
-      applyMove(state, action.payload);
+    addMove: (state, action: PayloadAction<TimestampedMove>) => {
+      state.cubeState = applyMove(state.cubeState, action.payload.move);
+      state.moves.push(action.payload);
     },
     resetCube: (state) => {
-      return getSolvedState();
+      state.cubeState = getSolvedState();
+      state.moves = [];
+      state.target = undefined;
     },
+    setTarget: (state, action: PayloadAction<CubeState>) => {
+      state.target = action.payload;
+    }
   },
 });
 
 // Actions
-export const { applyTurn, resetCube } = cubeSlice.actions;
+export const { addMove, resetCube } = cubeSlice.actions;
 
+// Helpers
+export function initialiseCubeBluetoothCallback(store: EnhancedStore) {
+  getCubeService().setCallback((state, moves, timestamps, source) => {
+    store.dispatch(addMove({ move: moves[0].trim(), timestamps }));
+  });
+}
--- src/state/store.ts
+++ src/state/store.ts
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { settingsSlice, settingsPersistanceMiddleware } from "./settings";
 import { connectionSlice, connectionMiddleware, disconnectMiddleware } from "./connection";
-import { cubeSlice } from "./cube";
+import { cubeSlice, initialiseCubeBluetoothCallback } from "./cube";
 import { promptSlice, promptResetOnSettingsChangeMiddleware, addSettingsToPromptResetMiddleware } from "./prompt";
 
 const store = configureStore({
@@ -20,6 +20,8 @@ const store = configureStore({
     addSettingsToPromptResetMiddleware,
   ),
 });
+
+initialiseCubeBluetoothCallback(store);
 
 export default store;
 

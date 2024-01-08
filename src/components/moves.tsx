650f87673cb713cd4d247133fa56ac160b08fd1c
AlphaSheep <brendan@alphasheep.co.za>
2024-01-08 09:49:46 +0200

    Only display last 25 moves

--- src/components/moves.tsx
+++ src/components/moves.tsx
@@ -15,5 +15,10 @@ export default function Moves() {
 
 function movesToString(moves: TimestampedMove[]): string {
   const alg = simplifyAlg(moves.map((move) => new Move(move.move)));
+
+  if (alg.length >= 25) {
+    return "..." + alg.slice(-25).map((move) => move.toString()).join(" ");
+  }
   return alg.map((move) => move.toString()).join(" ");
+
 }

b37178bfe0629fcb373064b2b805b689d9d9db51
AlphaSheep <brendan@alphasheep.co.za>
2024-01-16 14:43:15 +0200

    Reduce maximum moves to display to 20

--- src/components/moves.tsx
+++ src/components/moves.tsx
@@ -4,6 +4,8 @@ import { useAppSelector } from "../state/hooks";
 import { TimestampedMove, selectMoves } from "../state/cube.duck";
 import { Move, simplifyAlg } from "../lib/cube/cube";
 
+const MAX_MOVES = 20;
+
 export default function Moves() {
   const moves = useAppSelector(selectMoves);
   return (
@@ -16,8 +18,8 @@ export default function Moves() {
 function movesToString(moves: TimestampedMove[]): string {
   const alg = simplifyAlg(moves.map((move) => new Move(move.move)));
 
-  if (alg.length >= 25) {
-    return "..." + alg.slice(-25).map((move) => move.toString()).join(" ");
+  if (alg.length >= MAX_MOVES) {
+    return "..." + alg.slice(-MAX_MOVES).map((move) => move.toString()).join(" ");
   }
   return alg.map((move) => move.toString()).join(" ");
 

22ee3029b4b289ab2bc86c531eb5a2d2b43be786
AlphaSheep <brendan@alphasheep.co.za>
2024-01-16 14:45:29 +0200

    Simplify footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,13 +5,10 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export function Footer() {
   return (
-    <footer>
-      <p>
-        <a href="https://github.com/AlphaSheep/smartcube-3style" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faGithub} />
-          <span>Github</span>
-        </a>
-      </p>
-    </footer>
+    <a href="https://github.com/AlphaSheep/smartcube-3style" target="_blank" rel="noopener noreferrer">
+      <FontAwesomeIcon icon={faGithub} />
+      <span>Github</span>
+    </a>
   );
 }
+
